8851fbeee14f27683ce92c002d0bfe55d5ba9304
ilaria <ilaria.salutari@gmail.com>
2024-01-23 06:56:16 +0000

    now reads any traj format, with topology file

--- convert.py
+++ convert.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(
                     epilog='Thank you !')
 
 parser.add_argument('-f', '--traj_file', help='The input trajectory', required=True)
+parser.add_argument('-top', '--topology_file', help = 'Optional (depending on the traj format): a reference topology (.pdb or .gro format)', required=False)
 parser.add_argument('-o', '--out_file', help='Add a name for output', default='traj.xtc')
 parser.add_argument('-skip', '--stride', help = 'Save every n-th frame', type = int,  default = 1)
 parser.add_argument('-sel', '--selection', help = 'Select what to visualize using the MDtraj synthax between quotes (default: no water or ions)', default = 'not (resname HOH or name NA or name CL)')
@@ -26,11 +27,25 @@ out_dir_pdb = os.path.join(out_dir, pdb_name)
 
 ## Convert and save:
 
-frame0 = md.load_frame(dic_input['traj_file'], 0)
+if dic_input['traj_file'].endswith(".h5"):
 
-protein_ind = frame0.topology.select(dic_input['selection'])
-frame0 = frame0.atom_slice(protein_ind)
-frame0.save(out_dir_pdb)
+    frame0 = md.load_frame(dic_input['traj_file'], index=0)
 
-traj = md.load(dic_input['traj_file'], atom_indices = protein_ind, stride = dic_input['stride'])
-traj.save(dic_input['out_file'])
+    protein_ind = frame0.topology.select(dic_input['selection'])
+    frame0 = frame0.atom_slice(protein_ind)
+    frame0.save(out_dir_pdb)
+
+    traj = md.load(dic_input['traj_file'], atom_indices = protein_ind, stride = dic_input['stride'])
+    traj.save(dic_input['out_file'])
+
+else:
+
+    frame0 = md.load_frame(dic_input['traj_file'], top = dic_input['topology_file'], index=0)
+
+    protein_ind = frame0.topology.select(dic_input['selection'])
+    frame0 = frame0.atom_slice(protein_ind)
+    frame0.save(out_dir_pdb)
+
+    traj = md.load(dic_input['traj_file'], top = dic_input['topology_file'], atom_indices = protein_ind, stride = dic_input['stride'])
+    traj.save(dic_input['out_file'])
+
